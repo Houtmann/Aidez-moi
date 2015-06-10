b14da7d2857acf8ef26cb1912af54278742a60bd
Had <hhoutmann@gmail.com>
2015-06-10 10:52:10 +0200

    Amélioration de la barre de navigation

--- ticket/urls.py
+++ ticket/urls.py
@@ -23,7 +23,7 @@ __author__ = 'had'
 from django.conf.urls import patterns, url
 from django.conf import settings
 from django.conf.urls.static import static
-
+from django.contrib import admin
 
 urlpatterns = patterns('ticket.views',
 
