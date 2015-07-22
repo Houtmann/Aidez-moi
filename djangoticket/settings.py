58195b7125d79bee868a7d1fdda56cc07d8dc48f
Had <hhoutmann@gmail.com>
2015-07-22 17:15:12 +0200

    sauvegarder au lieu de soumettre sur le bouton

--- djangoticket/settings.py
+++ djangoticket/settings.py
@@ -89,21 +89,21 @@ ROOT_URLCONF = 'djangoticket.urls'
 
 WSGI_APPLICATION = 'djangoticket.wsgi.application'
 
-DATABASES = {
+"""DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
-}
+}"""
 
-"""DATABASES = {
+DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
         'OPTIONS': {
             'read_default_file': 'C:\Program Files\MariaDB 10.0\data\my.ini',
         },
     }
-}"""
+}
 
 
 # Internationalization
